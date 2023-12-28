2a8c8eb11e31db5dd55718c75a2aa51e8789a721
mahant <mahant@krishtechnolabs.com>
2023-12-28 12:18:51 +0530

    merge

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        dfsdfd  
+          
         <a
           className="App-link"
           href="https://reactjs.org"
